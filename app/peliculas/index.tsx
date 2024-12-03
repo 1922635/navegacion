5b98bcd651cd79e6a83629218ad1da972bb3e771
Clase <1922635@alu.murciaeduca.es>
2024-12-03 16:11:32 +0100

    peliculas.data añadido y titulos mostrados en peliculas/index

--- app/peliculas/index.tsx
+++ app/peliculas/index.tsx
@@ -1,15 +1,18 @@
-import { Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
+import { peliculas } from "@/data/peliculas.data"
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Peliculas</Text>
-    </View>
+    <FlatList data={peliculas}
+        keyExtractor={(item) => item.id}
+        renderItem={({item}) => {
+            return (
+                <View>
+                    <Text>{item.title}</Text>
+                </View>
+            );
+        }}>
+
+    </FlatList>
   );
 }
